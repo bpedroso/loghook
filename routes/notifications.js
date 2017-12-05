f45a781ad548e26afd95001ed3fe9d059d1f818a
brunopedrosoc4 <bruno_pedroso@carrefour.com>
2017-12-05 14:34:47 -0200

    Add emit function

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var io = require('socket.io')(http);
+var io = require('../bin/www');
 
 router.get('/', function(req, res, next) {
-    console.log('Receive request POST (host %s ip %s baseUrl %s)', req.hostname, req.ip, req.baseUrl);
+    console.log('Receive request GET (host %s ip %s baseUrl %s)', req.hostname, req.ip, req.baseUrl);
     res.sendFile(appRoot + '/views/html/index.html');
 });
 
 router.put('/', function(req, res, next) {
-    console.log('Receive request POST (host %s ip %s baseUrl %s)', req.hostname, req.ip, req.baseUrl);
-    io.emit('notificationEvt', { for: 'everyone' });
+    console.log('Receive request PUT (host %s ip %s baseUrl %s)', req.hostname, req.ip, req.baseUrl);
+    io.emitMessage('Receive request PUT');
 });
 
 module.exports = router;
