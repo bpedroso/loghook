98bae7ef02ecac06a401e22c3781adcd5d766fa1
brunopedrosoc4 <bruno_pedroso@carrefour.com>
2017-12-05 17:17:03 -0200

    Add http method to response entity

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,7 +4,8 @@ var app = require('../app');
 var list = [];
 
 var historySize = 100;
-var Notification = function (hostname, ip, baseUrl, query) {
+var Notification = function (httpmethod, hostname, ip, baseUrl, query) {
+    this.httpmethod = httpmethod;
     this.hostname = hostname;
     this.ip = ip;
     this.baseUrl = baseUrl;
@@ -34,7 +35,24 @@ router.put('/', function(req, res, next) {
         list.splice(0, 1);
     }
 
-    list.push(new Notification(req.hostname, req.ip, req.baseUrl, query));
+    list.push(new Notification('PUT', req.hostname, req.ip, req.baseUrl, query));
+    res.send();
+});
+
+router.post('/', function(req, res, next) {
+    console.log('Receive request PUT (host %s ip %s baseUrl %s)', req.hostname, req.ip, req.baseUrl);
+
+    var url = require('url');
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    app.emitMessage(query);
+
+    console.log(list.size);
+    if(list.length >= historySize) {
+        list.splice(0, 1);
+    }
+
+    list.push(new Notification('POST', req.hostname, req.ip, req.baseUrl, query));
     res.send();
 });
 
